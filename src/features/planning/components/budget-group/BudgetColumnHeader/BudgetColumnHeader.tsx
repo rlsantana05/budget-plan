daf6ce8ec828086caf17fdbc67c588df67cc70ed
leonardo Santana <rlsantana05@gmail.com>
2026-08-25 10:43:10 -0400

    rename planning column header: Assigned/Activity/Available -> Planned/Spent/Remaining

--- src/features/planning/components/budget-group/BudgetColumnHeader/BudgetColumnHeader.tsx
+++ src/features/planning/components/budget-group/BudgetColumnHeader/BudgetColumnHeader.tsx
@@ -4,9 +4,9 @@ export function BudgetColumnHeader() {
   return (
     <div className={classes.header} role="columnheader">
       <span>Category</span>
-      <span>Assigned</span>
-      <span>Activity</span>
-      <span>Available</span>
+      <span>Planned</span>
+      <span>Spent</span>
+      <span>Remaining</span>
       <span aria-hidden="true" />
     </div>
   );
